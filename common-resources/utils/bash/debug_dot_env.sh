e2e8d04548494598fa576cde3aa6b360afb30fce
sn-arun <sn.arun@gmail.com>
2025-02-10 11:27:19 +0530

    Initial Commit

---
+++ common-resources/utils/bash/debug_dot_env.sh
@@ -0,0 +1,8 @@
+if [ -f .env1 ]; then
+    echo ".env file exists"
+    export $(cat .env | xargs)
+    cat .env
+else
+    echo ".env file does not exist. Please debug previous jobs."
+    exit 1
+fi  
